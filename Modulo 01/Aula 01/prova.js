fc8c4af861f3e3adfdd52a541a469e6c2c1edee6
rubensparente <rubensparente@yahoo.com.br>
2022-08-26 16:37:29 -0300

    Primeira atualização

---
+++ Modulo 01/Aula 01/prova.js
@@ -0,0 +1,3 @@
+str = "Banana Laranja Morango";
+resultado = str.split("", 1);
+console.log(resultado);
---
+++ Modulo 03/estruturadecisao.js
@@ -0,0 +1,43 @@
+/*
+    Programa Javascript que demonstra estruturas de decisção
+*/
+
+//Criando variáveis var, let ou const
+/*
+let letra = 'Java Script';
+console.log(letra);
+
+let numero1 =10, numero2 = 25;
+
+console.log(numero1+numero2);
+console.log(numero1-numero2);
+console.log(numero1*numero2);
+console.log(numero1/numero2);
+
+
+console.log(numero1>numero2);
+console.log(numero1<=numero2);
+console.log(numero1==numero2);
+console.log(numero1!=numero2);
+
+letra = 10;
+if(numero1 === letra)
+    console.log('Eles são do mesmo tipo');
+else if(numero1 == letra)
+    console.log('Eles são iguais');
+else
+    console.log('Os dois não são do mesmo tipo');
+
+
+
+let opcao =3;
+switch(opcao){
+    case 1: console.log("ensino médio"); break;
+    case 2: console.log("Graduado"); break;
+    case 3: console.log("mestrado"); break;
+    case 4: console.log("doutorado"); break;
+    default: console.log("Escolaridade não definida.");
+}
+*/
+
+
